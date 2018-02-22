b8206251f1f2693296ef3af8227e9250f1165089
PavelBogatyrev1989 <34604949+PavelBogatyrev1989@users.noreply.github.com>
2018-02-22 05:54:58 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,5 +3,31 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  var counter = 0;
+  var base = [];
+
+  for (i = 0; i <= preferences.length; i++) {
+
+    if (preferences[i] != 0 && preferences[preferences[i] - 1] != 0 && preferences[preferences[preferences[i] - 1] - 1] != 0
+    ) {
+
+      var love = i + 1;
+      if (base.includes(love)) {
+        continue;
+      }
+      if (love == (preferences[preferences[preferences[i] - 1] - 1]) &&
+
+        (preferences[i] != preferences[preferences[i] - 1] &&
+          preferences[preferences[i] - 1] != preferences[preferences[preferences[i] - 1] - 1] &&
+          preferences[preferences[preferences[i] - 1] - 1] != preferences[i])
+      ) {
+        base.push(preferences[i]);
+        base.push(preferences[preferences[i] - 1]);
+        base.push(preferences[preferences[preferences[i] - 1] - 1]);
+        counter++;
+      }
+    }
+  }
+
+  return counter;
 };
